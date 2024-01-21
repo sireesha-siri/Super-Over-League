f5e5298c6d100f129f1e153bcceb7247551c07ed
siri <agurusireeshaedu@gmail.com>
2024-01-21 15:38:00 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="super-over-league-background-container">
+    <h1 className="super-over-league-heading">Super Over League</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="logo-design"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="logo-design"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
